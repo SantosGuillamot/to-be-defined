a7d5de3536002a552bfd0627d6d83da5469c6593
Mario Santos <santosguillamot@gmail.com>
2024-11-15 02:33:16 +0100

    Init repo

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name:       To Be Defined
+ * Version:           1.0.0
+ * Requires at least: 6.7
+ * Requires PHP:      5.6
+ * Description:       Plugin to test creating blocks or other WordPress features in the browser.
+ * Author:            Mario Santos
+ * License:           GPL-3.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       santosguillamot
+ */
